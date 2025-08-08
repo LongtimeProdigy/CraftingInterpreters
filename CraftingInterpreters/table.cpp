cadead7cd7aaaec161481e859de01b6f7e596d27
LongtimeProdigy <dhflaos3@gmail.com>
2025-08-08 17:38:14 +0900

    30. Optimization

--- CraftingInterpreters/table.cpp
+++ CraftingInterpreters/table.cpp
@@ -23,7 +23,8 @@ void freeTable(Table* table)
 
 static Entry* findEntry(Entry* entries, int capacity, ObjString* key)
 {
-	uint32_t index = key->hash % capacity;
+	//uint32_t index = key->hash % capacity;
+	uint32_t index = key->hash & (capacity - 1);
 	Entry* tombstone = NULL;
 
 	for (;;)
@@ -48,7 +49,8 @@ static Entry* findEntry(Entry* entries, int capacity, ObjString* key)
 			return entry;
 		}
 
-		index = (index + 1) % capacity;
+		//index = (index + 1) % capacity;
+		index = (index + 1) & (capacity - 1);
 	}
 }
 
@@ -136,7 +138,8 @@ ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t
 {
 	if (table->count == 0) return NULL;
 
-	uint32_t index = hash % table->capacity;
+	//uint32_t index = hash % table->capacity;
+	uint32_t index = hash & (table->capacity - 1);
 	for (;;)
 	{
 		Entry* entry = &table->entries[index];
@@ -149,7 +152,8 @@ ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t
 			return entry->key;
 		}
 
-		index = (index + 1) % table->capacity;
+		//index = (index + 1) % table->capacity;
+		index = (index + 1) & (table->capacity - 1);
 	}
 }
 
--- CraftingInterpreters/value.cpp
+++ CraftingInterpreters/value.cpp
@@ -33,6 +33,24 @@ void freeValueArray(ValueArray* array)
 
 void printValue(Value value)
 {
+#ifdef NAN_BOXING
+	if (IS_BOOL(value))
+	{
+		printf(AS_BOOL(value) ? "true" : "false");
+	}
+	else if (IS_NIL(value))
+	{
+		printf("nil");
+	}
+	else if (IS_NUMBER(value))
+	{
+		printf("%g", AS_NUMBER(value));
+	}
+	else if (IS_OBJ(value))
+	{
+		printObject(value);
+	}
+#else
 	switch (value.type)
 	{
 	case VAL_BOOL:
@@ -46,10 +64,20 @@ void printValue(Value value)
 		break;
 	case VAL_OBJ: printObject(value); break;
 	}
+#endif
 }
 
 bool valuesEqual(Value a, Value b)
 {
+#ifdef NAN_BOXING
+	// for Nan != Nan
+	if (IS_NUMBER(a) && IS_NUMBER(b))
+	{
+		return AS_NUMBER(a) == AS_NUMBER(b);
+	}
+
+	return a == b;
+#else
 	if (a.type != b.type) return false;
 	switch (a.type)
 	{
@@ -59,4 +87,5 @@ bool valuesEqual(Value a, Value b)
 	case VAL_OBJ: return AS_OBJ(a) == AS_OBJ(b);
 	default: return false; // ������� �ʴ� �ڵ�
 	}
+#endif
 }
